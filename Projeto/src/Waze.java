edda52898bab1453b9e9cdc5609649f8ebcad7c6
MarcosUeda42 <m.ueda@unifesp.br>
2026-04-18 10:38:13 -0300

    adicionando gps waze e testando na main

---
+++ Projeto/src/Waze.java
@@ -0,0 +1,7 @@
+public class Waze {
+    
+    public void bestRoute(){
+        System.out.println("Waze: Conferindo tráfego...");
+        System.out.println("Waze: Calculando melhor rota...");
+    }
+}
