6e5d863aaa084499b6632deb8dc8ff5e9164e172
MarcosUeda42 <m.ueda@unifesp.br>
2026-04-19 10:04:14 -0300

    criando fachada para corridas usando gps google

---
+++ Projeto/src/CreateRun.java
@@ -0,0 +1,34 @@
+public class CreateRun {
+    
+    public void createLuxGoogle(double price) {
+
+        AdaptGPSGoogle google = new AdaptGPSGoogle(new GoogleMaps());
+        GPSSystem system = new GPSSystem();
+
+        CarSearch cs = new CarSearch();
+        CarSearchProxy proxy = new CarSearchProxy(cs);
+
+        Car car1 = CarFactory.createCar("lux");
+
+        proxy.getCar("lux");
+        system.initGPS(google);
+        car1.startRun();
+        System.out.println("Preço da viagem: " + car1.getPrice(price, 100) + " reais");
+    }
+
+    public void createEcoGoogle(double price) {
+
+        AdaptGPSGoogle google = new AdaptGPSGoogle(new GoogleMaps());
+        GPSSystem system = new GPSSystem();
+
+        CarSearch cs = new CarSearch();
+        CarSearchProxy proxy = new CarSearchProxy(cs);
+
+        Car car1 = CarFactory.createCar("eco");
+
+        proxy.getCar("eco");
+        system.initGPS(google);
+        car1.startRun();
+        System.out.println("Preço da viagem: " + car1.getPrice(price, 100) + " reais");
+    }
+}
