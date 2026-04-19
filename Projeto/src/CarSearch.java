40c8adee8822ec7cb961251d6185717396878802
MarcosUeda42 <m.ueda@unifesp.br>
2026-04-19 11:04:48 -0300

    criando strategy para busca de corrida

--- Projeto/src/CarSearch.java
+++ Projeto/src/CarSearch.java
@@ -1,5 +1,15 @@
 public class CarSearch implements CarSearchInterface {
     
+    SearchStrategy searchStrategy;
+
+    public CarSearch() {
+        this(new FastSearch());
+    }
+
+    public CarSearch(SearchStrategy searchStrategy) {
+        this.searchStrategy = searchStrategy;
+    }
+
     public Car getCar(String type) {
         System.out.println("Carro encontrado: " + type);
         if (type.equalsIgnoreCase("lux")) {
---
+++ Projeto/src/SearchStrategy.java
@@ -0,0 +1,3 @@
+abstract class SearchStrategy {
+    abstract void search();
+}
